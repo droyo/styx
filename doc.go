201ba8747345788e1119a997b4e882747b3e46e9
David Arroyo <darroyo@constantcontact.com>
2015-11-21 17:23:11 -0500

    remove prototype

--- doc.go
+++ doc.go
@@ -5,15 +5,3 @@ The styx package allows for the implementation and usage of distributed
 file systems via the 9P filesystem protocol.
 */
 package styx
-
-import "aqwari.net/net/styx/styxproto"
-
-type Server interface {
-	Attach(styxproto.Tattach) error
-	Auth(styxproto.Tauth) error
-	Open(styxproto.Topen) error
-	Read(styxproto.Tread) error
-}
-
-type Filesystem interface {
-}
