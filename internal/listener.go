db628b795fb1042f347b085ef9215ba1ae23767b
David <droyo@aqwari.net>
2015-11-29 21:58:44 -0500

    add rudimentary test of 9P server functionality

---
+++ internal/listener.go
@@ -0,0 +1,77 @@
+package internal
+
+import (
+	"errors"
+	"net"
+	"sync"
+)
+
+var (
+	errClosed = errors.New("listener closed")
+)
+
+// PipeListener is a net.Listener that does not need permission to
+// bind to a port or create a socket file. Useful for testing in heavily
+// sandboxed environments or intra-process communication.
+type PipeListener struct {
+	once     sync.Once
+	incoming chan net.Conn
+	shutdown chan struct{}
+}
+
+func (l *PipeListener) init() {
+	l.once.Do(func() {
+		l.incoming = make(chan net.Conn)
+		l.shutdown = make(chan struct{})
+	})
+}
+
+// Accept accepts a new connection on a PipeListener.
+// Accept blocks until a new connection is made or the
+// PipeListener is closed.
+func (l *PipeListener) Accept() (net.Conn, error) {
+	l.init()
+	select {
+	case c := <-l.incoming:
+		return c, nil
+	case <-l.shutdown:
+		return nil, errClosed
+	}
+}
+
+func (l *PipeListener) Dial() (net.Conn, error) {
+	l.init()
+	x, y := net.Pipe()
+	select {
+	case <-l.shutdown:
+		x.Close()
+		y.Close()
+		return nil, errClosed
+	case l.incoming <- x:
+		return y, nil
+	}
+	panic("not reached")
+}
+
+// Close closes a PipeListener. The returned error will always
+// be nil.
+func (l *PipeListener) Close() error {
+	l.init()
+	select {
+	case <-l.shutdown:
+		// avoiding a panic on double close here
+	default:
+		close(l.shutdown)
+	}
+	return nil
+}
+
+type dummyAddress struct{}
+
+func (dummyAddress) String() string  { return "" }
+func (dummyAddress) Network() string { return "" }
+
+func (l *PipeListener) Addr() net.Addr {
+	l.init()
+	return dummyAddress{}
+}
--- server_test.go
+++ server_test.go
@@ -1,1 +1,49 @@
 package styx
+
+import (
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"aqwari.net/net/styx/internal"
+)
+
+type dialee interface {
+	Dial() (net.Conn, error)
+}
+
+type testLogger testing.T
+
+func (t *testLogger) Printf(format string, v ...interface{}) {
+	t.Logf(format, v...)
+}
+
+func sendTraffic(t *testing.T, ln *internal.PipeListener) {
+	c, err := ln.Dial()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	file, err := os.Open(filepath.Join("styxproto", "testdata", "sample.client.9p"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	io.Copy(c, file)
+}
+
+func TestServerBasic(t *testing.T) {
+	srv := Server{
+		ErrorLog: (*testLogger)(t),
+		TraceLog: (*testLogger)(t),
+	}
+	var ln internal.PipeListener
+	defer ln.Close()
+
+	go func() {
+		t.Log(srv.Serve(&ln))
+	}()
+	sendTraffic(t, &ln)
+}
