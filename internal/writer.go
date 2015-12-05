29a6093ada687713c37dbeaeba3561b0b5b238bb
David Arroyo <darroyo@constantcontact.com>
2015-12-04 20:54:09 -0500

    Add a TxWriter utility type.
    For the multi-threaded server implementation, multiple threads may
    want to write a message at any given time. The TxWriter utility type
    provides a way to isolate sets of writes so that we don't mangle the
    data stream.
    
    It is common to see types that put a mutex around the Write method
    to achieve a similar goal. That was not enough in my case, because
    the functions that create messages make many Write calls. Arranging
    to have the whole message in a single Write call is more work than
    implementing these transactions.

--- internal/writer.go
+++ internal/writer.go
@@ -1,6 +1,9 @@
 package internal
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
 // An ErrWriter can be used to defer error checking when
 // doing several successive writes to an underlying
@@ -36,4 +39,43 @@ func (w *ErrWriter) WriteByte(v byte) error {
 	w.N += n
 	w.Err = err
 	return err
+}
+
+// A TxWriter provides allows for isolation of writes or sets of writes
+// on an io.Writer.
+type TxWriter struct {
+	W  io.Writer
+	mu sync.Mutex
+}
+
+// Write writes data to w's underlying writer. Individual Write
+// calls are isolated from any concurrent Writes or sets of
+// Writes made within a transaction.
+func (w *TxWriter) Write(p []byte) (int, error) {
+	w.mu.Lock()
+	n, err := w.W.Write(p)
+	w.mu.Unlock()
+	return n, err
+}
+
+// A Tx allows for a set of multiple Write calls to be isolated from
+// other Writes. The returned io.WriteCloser will begin a transaction
+// on its first call to Write, and end the transaction when closed.
+func (w *TxWriter) Tx() io.WriteCloser {
+	return &tx{TxWriter: w}
+}
+
+type tx struct {
+	*TxWriter
+	once sync.Once
+}
+
+func (w *tx) Write(p []byte) (int, error) {
+	w.once.Do(func() { w.mu.Lock() })
+	return w.TxWriter.W.Write(p)
+}
+
+func (w *tx) Close() error {
+	w.mu.Unlock()
+	return nil
 }
