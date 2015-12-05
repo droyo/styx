502a0c056423008b6b8533484fc94a6314232504
David Arroyo <darroyo@constantcontact.com>
2015-12-04 20:45:59 -0500

    Remove `QidType`
    This type had no messages and we do not yet have a use
    for a QidType. I do not intend to expose it in the higher
    level package. If there comes a need for it, I can put `QidType`
    back.

--- styxproto/qid.go
+++ styxproto/qid.go
@@ -27,7 +27,7 @@ func NewQid(buf []byte, qtype uint8, version uint32, path uint64) (Qid, []byte, 
 }
 
 // Type returns the type of a file (directory, etc)
-func (q Qid) Type() QidType { return QidType(q[0]) }
+func (q Qid) Type() uint8 { return q[0] }
 
 // Version is a version number for a file; typically, it is incremented
 // every time a file is modified. By convention, synthetic files usually
@@ -45,17 +45,15 @@ func (q Qid) String() string {
 	return fmt.Sprintf("type=%d ver=%d path=%x", q.Type(), q.Version(), q.Path())
 }
 
-// A QidType represents the type of a file (directory, etc.), represented
+// A Qid's type field represents the type of a file (directory, etc.), represented
 // as a bit vector corresponding to the high 8 bits of the file's mode
 // word.
-type QidType uint8
-
 const (
-	QTDIR    QidType = 0x80 // directories
-	QTAPPEND QidType = 0x40 // append only files
-	QTEXCL   QidType = 0x20 // exclusive use files
-	QTMOUNT  QidType = 0x10 // mounted channel
-	QTAUTH   QidType = 0x08 // authentication file (afid)
-	QTTMP    QidType = 0x04 // non-backed-up file
-	QTFILE   QidType = 0x00
+	QTDIR    = 0x80 // directories
+	QTAPPEND = 0x40 // append only files
+	QTEXCL   = 0x20 // exclusive use files
+	QTMOUNT  = 0x10 // mounted channel
+	QTAUTH   = 0x08 // authentication file (afid)
+	QTTMP    = 0x04 // non-backed-up file
+	QTFILE   = 0x00
 )
