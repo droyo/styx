8e58b36e08f04a6bc7a9ff52f5e73e72b8a043a4
David <droyo@aqwari.net>
2015-12-06 00:18:01 -0500

    Add Close semantix to TxWriter
    Before this commit you could continue writing to a TxWriter.Tx after
    closing it, and it would happily corrupt your data stream. Now once
    a transaction is closed, it's closed for good.

--- internal/wire/txwriter.go
+++ internal/wire/txwriter.go
@@ -1,8 +1,15 @@
 package wire
 
 import (
+	"errors"
 	"io"
 	"sync"
+	"sync/atomic"
+)
+
+var (
+	errDoubleClose = errors.New("Close on closed TxWriter.Tx")
+	errClosedWrite = errors.New("Write on closed TxWriter.Tx")
 )
 
 // A TxWriter provides allows for isolation of writes or sets of writes
@@ -25,21 +32,30 @@ func (w *TxWriter) Write(p []byte) (int, error) {
 // A Tx allows for a set of multiple Write calls to be isolated from
 // other Writes. The returned io.WriteCloser will begin a transaction
 // on its first call to Write, and end the transaction when closed.
+// The returned Writer can only be used from a single goroutine.
 func (w *TxWriter) Tx() io.WriteCloser {
 	return &tx{TxWriter: w}
 }
 
 type tx struct {
 	*TxWriter
-	once sync.Once
+	closed uint32
+	once   sync.Once
 }
 
 func (w *tx) Write(p []byte) (int, error) {
 	w.once.Do(func() { w.mu.Lock() })
+	closed := atomic.LoadUint32(&w.closed)
+	if closed == 1 {
+		return 0, errClosedWrite
+	}
 	return w.TxWriter.W.Write(p)
 }
 
 func (w *tx) Close() error {
+	if swapped := atomic.CompareAndSwapUint32(&w.closed, 0, 1); !swapped {
+		return errDoubleClose
+	}
 	w.mu.Unlock()
 	return nil
 }
