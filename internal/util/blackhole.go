80d7192bb18c64645925b8844738a041051ba5c6
David Arroyo <darroyo@constantcontact.com>
2015-12-19 21:21:09 -0700

    Implement the `io.ReadWriteCloser` interface for `util.Blackhole`

--- internal/util/blackhole.go
+++ internal/util/blackhole.go
@@ -2,9 +2,10 @@ package util
 
 import "io"
 
-// A BlackHole is an io.ReadWriter that accepts an infinite
+// A BlackHole is an io.ReadWriteCloser that accepts an infinite
 // amount of input and produces no output.
 type BlackHole struct{}
 
 func (BlackHole) Read(p []byte) (int, error)  { return 0, io.EOF }
 func (BlackHole) Write(p []byte) (int, error) { return len(p), nil }
+func (BlackHole) Close() error                { return nil }
