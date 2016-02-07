8944391b06b44769f0ddc80963e4d0b46c3da21d
David Arroyo <darroyo@constantcontact.com>
2016-02-07 16:13:17 -0500

    Defer error checking for styxproto.Encoder methods
    I found I was ignoring errors on Encoder methods in higher-level
    code, because I knew if a problem occured when writing to the
    connection, my decoding loop would break. With this commit, I have
    removed the error return value from most of the encoding functions.
    
    Special notes:
    
    - Where input parameters can be too long, they are truncated.
    - If a `Stat` param was not produced by `NewStat` or a `Decoder`,
      it can raise a panic when passed to the encoding functions that
      take a `Stat`.
    - For `Rread`, the non-IO error was avoided by breaking large
      input up into `Msize`-sized chunks.
    - I could not find a satisfactory way to avoid an error return for
      some functions, such as `Twrite` and `Twalk`. For now, they continue
      to return an error.

--- styxproto/encoder.go
+++ styxproto/encoder.go
@@ -12,8 +12,9 @@ import (
 // An Encoder writes 9P messages to an underlying
 // io.Writer.
 type Encoder struct {
-	w  *wire.TxWriter
-	ew *util.ErrWriter
+	Msize int64
+	w     *wire.TxWriter  // writes to ew
+	ew    *util.ErrWriter // wraps the connection
 }
 
 // NewEncoder creates a new Encoder that writes 9P messages
@@ -34,10 +35,11 @@ func (enc *Encoder) Err() error {
 }
 
 // Tversion writes a Tversion message to the underlying io.Writer.
-// The Tag of the written message will be NoTag.
-func (enc *Encoder) Tversion(msize uint32, version string) error {
+// The Tag of the written message will be NoTag. If the version string
+// is longer than MaxVersionLen, it is truncated.
+func (enc *Encoder) Tversion(msize uint32, version string) {
 	if len(version) > MaxVersionLen {
-		return errLongVersion
+		version = version[:MaxVersionLen]
 	}
 	size := uint32(minSizeLUT[msgTversion] + len(version))
 
@@ -46,13 +48,13 @@ func (enc *Encoder) Tversion(msize uint32, version string) error {
 
 	pheader(tx, size, msgTversion, NoTag, msize)
 	pstring(tx, version)
-	return enc.Err()
 }
 
 // Rversion writes an Rversion message to the underlying io.Writer.
-func (enc *Encoder) Rversion(msize uint32, version string) error {
+// If the version string is longer than MaxVerisonLen, it is truncated.
+func (enc *Encoder) Rversion(msize uint32, version string) {
 	if len(version) > MaxVersionLen {
-		return errLongVersion
+		version = version[:MaxVersionLen]
 	}
 	size := uint32(minSizeLUT[msgRversion] + len(version))
 
@@ -61,18 +63,17 @@ func (enc *Encoder) Rversion(msize uint32, version string) error {
 
 	pheader(tx, size, msgRversion, NoTag, msize)
 	pstring(tx, version)
-	return enc.Err()
 }
 
 // Tauth writes a Tauth message to enc's underlying io.Writer. The uname
-// and aname parameters must be shorter than MaxUidLen and MaxAttachLen,
-// respectively.
-func (enc *Encoder) Tauth(tag uint16, afid uint32, uname, aname string) error {
+// and aname parameters will be truncated if they are longer than MaxUidLen
+// and MaxAttachLen, respectively.
+func (enc *Encoder) Tauth(tag uint16, afid uint32, uname, aname string) {
 	if len(uname) > MaxUidLen {
-		return errLongUsername
+		uname = uname[:MaxUidLen]
 	}
 	if len(aname) > MaxAttachLen {
-		return errLongAname
+		aname = aname[:MaxAttachLen]
 	}
 	size := uint32(minSizeLUT[msgTauth] + len(uname) + len(aname))
 
@@ -81,11 +82,10 @@ func (enc *Encoder) Tauth(tag uint16, afid uint32, uname, aname string) error {
 
 	pheader(tx, size, msgTauth, tag, afid)
 	pstring(tx, uname, aname)
-	return enc.Err()
 }
 
 // Rauth writes a new Rauth message to the underlying io.Writer.
-func (enc *Encoder) Rauth(tag uint16, qid Qid) error {
+func (enc *Encoder) Rauth(tag uint16, qid Qid) {
 	size := uint32(maxSizeLUT[msgRauth])
 
 	tx := enc.w.Tx()
@@ -93,17 +93,18 @@ func (enc *Encoder) Rauth(tag uint16, qid Qid) error {
 
 	pheader(tx, size, msgRauth, tag)
 	pqid(tx, qid)
-	return enc.Err()
 }
 
-// Tattach writes a new Tattach message to the underlying io.Writer. If the client does not want
-// to authenticate, afid should be NoFid.
-func (enc *Encoder) Tattach(tag uint16, fid, afid uint32, uname, aname string) error {
+// Tattach writes a new Tattach message to the underlying io.Writer.
+// If the client does not want to authenticate, afid should be NoFid.
+// The uname and aname parameters will be truncated if they are longer
+// than MaxUidLen and MaxAttachLen, respectively.
+func (enc *Encoder) Tattach(tag uint16, fid, afid uint32, uname, aname string) {
 	if len(uname) > MaxUidLen {
-		return errLongUsername
+		uname = uname[:MaxUidLen]
 	}
 	if len(aname) > MaxAttachLen {
-		return errLongAname
+		aname = aname[:MaxAttachLen]
 	}
 	size := uint32(minSizeLUT[msgTattach] + len(uname) + len(aname))
 
@@ -112,11 +113,10 @@ func (enc *Encoder) Tattach(tag uint16, fid, afid uint32, uname, aname string) e
 
 	pheader(tx, size, msgTattach, tag, fid, afid)
 	pstring(tx, uname, aname)
-	return enc.Err()
 }
 
 // Rattach writes a new Rattach message to the underlying io.Writer.
-func (enc *Encoder) Rattach(tag uint16, qid Qid) error {
+func (enc *Encoder) Rattach(tag uint16, qid Qid) {
 	size := uint32(maxSizeLUT[msgRattach])
 
 	tx := enc.w.Tx()
@@ -124,14 +124,13 @@ func (enc *Encoder) Rattach(tag uint16, qid Qid) error {
 
 	pheader(tx, size, msgRattach, tag)
 	pqid(tx, qid)
-	return enc.Err()
 }
 
 // Rerror writes a new Rerror message to the underlying io.Writer. Errfmt may
 // be a printf-style format string, with values filled in from the
 // argument list v. If the error string is longer than MaxErrorLen
 // bytes, it is truncated.
-func (enc *Encoder) Rerror(tag uint16, errfmt string, v ...interface{}) error {
+func (enc *Encoder) Rerror(tag uint16, errfmt string, v ...interface{}) {
 	ename := errfmt
 	if len(v) > 0 {
 		ename = fmt.Sprintf(errfmt, v...)
@@ -146,12 +145,10 @@ func (enc *Encoder) Rerror(tag uint16, errfmt string, v ...interface{}) error {
 
 	pheader(tx, size, msgRerror, tag)
 	pstring(tx, ename)
-
-	return enc.Err()
 }
 
 // Tflush writes a new Tflush message to the underlying io.Writer.
-func (enc *Encoder) Tflush(tag, oldtag uint16) error {
+func (enc *Encoder) Tflush(tag, oldtag uint16) {
 	size := uint32(maxSizeLUT[msgTflush])
 
 	tx := enc.w.Tx()
@@ -159,18 +156,16 @@ func (enc *Encoder) Tflush(tag, oldtag uint16) error {
 
 	pheader(tx, size, msgTflush, tag)
 	puint16(tx, oldtag)
-	return enc.Err()
 }
 
 // Rflush writes a new Rflush message to the underlying io.Writer.
-func (enc *Encoder) Rflush(tag uint16) error {
+func (enc *Encoder) Rflush(tag uint16) {
 	size := uint32(maxSizeLUT[msgRflush])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgRflush, tag)
-	return enc.Err()
 }
 
 // Twalk writes a new Twalk message to the underlying io.Writer. An error is returned
@@ -196,7 +191,7 @@ func (enc *Encoder) Twalk(tag uint16, fid, newfid uint32, wname ...string) error
 	puint16(tx, uint16(len(wname)))
 	pstring(tx, wname...)
 
-	return enc.Err()
+	return nil
 }
 
 // Rwalk writes a new Rwalk message to the underlying io.Writer. An error is returned if wqid
@@ -214,11 +209,11 @@ func (enc *Encoder) Rwalk(tag uint16, wqid ...Qid) error {
 	puint16(tx, uint16(len(wqid)))
 	pqid(tx, wqid...)
 
-	return enc.Err()
+	return nil
 }
 
 // NewTopen writes a new Topen message to the underlying io.Writer.
-func (enc *Encoder) Topen(tag uint16, fid uint32, mode uint8) error {
+func (enc *Encoder) Topen(tag uint16, fid uint32, mode uint8) {
 	size := uint32(maxSizeLUT[msgTopen])
 
 	tx := enc.w.Tx()
@@ -226,12 +221,10 @@ func (enc *Encoder) Topen(tag uint16, fid uint32, mode uint8) error {
 
 	pheader(tx, size, msgTopen, tag, fid)
 	puint8(tx, mode)
-
-	return enc.Err()
 }
 
 // Ropen writes a new Ropen message to the underlying io.Writer.
-func (enc *Encoder) Ropen(tag uint16, qid Qid, iounit uint32) error {
+func (enc *Encoder) Ropen(tag uint16, qid Qid, iounit uint32) {
 	size := uint32(maxSizeLUT[msgRopen])
 
 	tx := enc.w.Tx()
@@ -240,14 +233,13 @@ func (enc *Encoder) Ropen(tag uint16, qid Qid, iounit uint32) error {
 	pheader(tx, size, msgRopen, tag)
 	pqid(tx, qid)
 	puint32(tx, iounit)
-
-	return enc.Err()
 }
 
-// Tcreate writes a new Tcreate message to the underlying io.Writer.
-func (enc *Encoder) Tcreate(tag uint16, fid uint32, name string, perm uint32, mode uint8) error {
+// Tcreate writes a new Tcreate message to the underlying io.Writer. If
+// name is longer than MaxFilenameLen, it is truncated.
+func (enc *Encoder) Tcreate(tag uint16, fid uint32, name string, perm uint32, mode uint8) {
 	if len(name) > MaxFilenameLen {
-		return errLongFilename
+		name = name[:MaxFilenameLen]
 	}
 	size := uint32(minSizeLUT[msgTcreate] + len(name))
 
@@ -258,12 +250,10 @@ func (enc *Encoder) Tcreate(tag uint16, fid uint32, name string, perm uint32, mo
 	pstring(tx, name)
 	puint32(tx, perm)
 	puint8(tx, mode)
-
-	return enc.Err()
 }
 
 // Rcreate writes a new Rcreate message to the underlying io.Writer.
-func (enc *Encoder) Rcreate(tag uint16, qid Qid, iounit uint32) error {
+func (enc *Encoder) Rcreate(tag uint16, qid Qid, iounit uint32) {
 	size := uint32(maxSizeLUT[msgRcreate])
 
 	tx := enc.w.Tx()
@@ -272,8 +262,6 @@ func (enc *Encoder) Rcreate(tag uint16, qid Qid, iounit uint32) error {
 	pheader(tx, size, msgRcreate, tag)
 	pqid(tx, qid)
 	puint32(tx, iounit)
-
-	return enc.Err()
 }
 
 // Tread writes a new Tread message to the underlying io.Writer. An error is returned
@@ -290,38 +278,33 @@ func (enc *Encoder) Tread(tag uint16, fid uint32, offset, count int64) error {
 	pheader(tx, size, msgTread, tag, fid)
 	puint64(tx, uint64(offset))
 	puint32(tx, uint32(count))
-	return enc.Err()
+	return nil
 }
 
-// Rread writes a new Rread message to the underlying io.Writer. An error is returned
-// if the message cannot fit inside a single 9P message.
-func (enc *Encoder) Rread(tag uint16, data []byte) error {
-	if math.MaxUint32-minSizeLUT[msgTwrite] < len(data) {
-		return errTooBig
+// Rread writes a new Rread message to the underlying io.Writer. An
+// error is returned if the message cannot fit inside a single 9P
+// message. If len(data) is greater than the Encoder's Msize, it is
+// broken up into multiple Rread messages.
+func (enc *Encoder) Rread(tag uint16, data []byte) {
+	msize := enc.Msize
+	if msize < MinBufSize {
+		msize = MinBufSize
 	}
-	size := uint32(minSizeLUT[msgRread]) + uint32(len(data))
+	msize -= int64(minSizeLUT[msgRread])
+	for len(data) > 0 {
+		chunk := data
+		if int64(len(data)) > msize {
+			chunk = data[:msize]
+		}
 
-	tx := enc.w.Tx()
-	defer tx.Close()
+		size := uint32(minSizeLUT[msgRread]) + uint32(len(chunk))
+		tx := enc.w.Tx()
+		pheader(tx, size, msgRread, tag, uint32(len(chunk)))
+		tx.Write(chunk)
+		tx.Close()
 
-	pheader(tx, size, msgRread, tag, uint32(len(data)))
-	tx.Write(data)
-	return enc.Err()
-}
-
-// Rreader obtains the data portion of an Rread message from an io.Reader.
-func (enc *Encoder) Rreader(tag uint16, count int64, r io.Reader) error {
-	if int64(math.MaxUint32-minSizeLUT[msgRread]) < count {
-		return errTooBig
+		data = data[len(chunk):]
 	}
-	size := uint32(minSizeLUT[msgRread]) + uint32(count)
-
-	tx := enc.w.Tx()
-	defer tx.Close()
-
-	pheader(tx, size, msgRread, tag, uint32(count))
-	_, err := io.CopyN(tx, r, int64(size))
-	return err
 }
 
 // Twrite writes a Twrite message to the underlying io.Writer. An error is returned
@@ -342,29 +325,12 @@ func (enc *Encoder) Twrite(tag uint16, fid uint32, offset int64, data []byte) er
 	return enc.Err()
 }
 
-// Twriter obtains the data portion of a Twrite message from an io.Reader.
-func (enc *Encoder) Twriter(tag uint16, fid uint32, offset, count int64, r io.Reader) error {
-	if int64(math.MaxUint32-minSizeLUT[msgTwrite]) < count {
-		return errTooBig
-	}
-	size := uint32(minSizeLUT[msgTwrite]) + uint32(count)
-
-	tx := enc.w.Tx()
-	defer tx.Close()
-
-	pheader(tx, size, msgTread, tag, fid)
-	puint64(tx, uint64(offset))
-	puint32(tx, uint32(count))
-	_, err := io.CopyN(tx, r, int64(size))
-	return err
-}
-
-// Rwrite writes an Rwrite message to the underlying io.Writer. An error is returned
-// if count is greater than the maximum value of a 32-bit unsigned
-// integer.
-func (enc *Encoder) Rwrite(tag uint16, count int64) error {
+// Rwrite writes an Rwrite message to the underlying io.Writer.
+// If count is greater than the maximum value of a 32-bit unsigned
+// integer, a run-time panic occurs.
+func (enc *Encoder) Rwrite(tag uint16, count int64) {
 	if count > math.MaxUint32 {
-		return errMaxCount
+		panic(errMaxCount)
 	}
 	size := uint32(maxSizeLUT[msgRwrite])
 
@@ -372,72 +338,67 @@ func (enc *Encoder) Rwrite(tag uint16, count int64) error {
 	defer tx.Close()
 
 	pheader(tx, size, msgRwrite, tag, uint32(count))
-
-	return enc.Err()
 }
 
 // Tclunk writes a Tclunk message to the underlying io.Writer.
-func (enc *Encoder) Tclunk(tag uint16, fid uint32) error {
+func (enc *Encoder) Tclunk(tag uint16, fid uint32) {
 	size := uint32(maxSizeLUT[msgTclunk])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgTclunk, tag, fid)
-	return enc.Err()
 }
 
 // Rclunk writes an Rclunk message to the underlying io.Writer.
-func (enc *Encoder) Rclunk(tag uint16) error {
+func (enc *Encoder) Rclunk(tag uint16) {
 	size := uint32(maxSizeLUT[msgRclunk])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgRclunk, tag)
-	return enc.Err()
 }
 
 // Tremove writes a Tremove message to the underlying io.Writer.
-func (enc *Encoder) Tremove(tag uint16, fid uint32) error {
+func (enc *Encoder) Tremove(tag uint16, fid uint32) {
 	size := uint32(maxSizeLUT[msgTremove])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgTremove, tag, fid)
-	return enc.Err()
 }
 
 // Rremove writes an Rremove message to the underlying io.Writer.
-func (enc *Encoder) Rremove(tag uint16) error {
+func (enc *Encoder) Rremove(tag uint16) {
 	size := uint32(maxSizeLUT[msgRremove])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgRremove, tag)
-	return enc.Err()
 }
 
 // Tstat writes a Tstat message to the underlying io.Writer.
-func (enc *Encoder) Tstat(tag uint16, fid uint32) error {
+func (enc *Encoder) Tstat(tag uint16, fid uint32) {
 	size := uint32(maxSizeLUT[msgTstat])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgTstat, tag, fid)
-	return enc.Err()
 }
 
 // Rstat writes an Rstat message to the underlying io.Writer.
-func (enc *Encoder) Rstat(tag uint16, stat Stat) error {
+// If the Stat is larger than the maximum size allowed by
+// the NewStat function, a run-time panic occurs.
+func (enc *Encoder) Rstat(tag uint16, stat Stat) {
 	if len(stat) > maxStatLen {
-		return errLongStat
+		panic(errLongStat)
 	}
 	if len(stat) < minStatLen {
-		return errShortStat
+		panic(errShortStat)
 	}
 	size := uint32((minSizeLUT[msgRstat] - minStatLen) + len(stat))
 
@@ -446,17 +407,17 @@ func (enc *Encoder) Rstat(tag uint16, stat Stat) error {
 
 	pheader(tx, size, msgRstat, tag)
 	pbyte(tx, stat)
-
-	return enc.Err()
 }
 
 // Twstat writes a Twstat message to the underlying io.Writer.
-func (enc *Encoder) Twstat(tag uint16, fid uint32, stat Stat) error {
+// If the Stat is larger than the maximum size allowed by the
+// NewStat function, a run-time panic occurs.
+func (enc *Encoder) Twstat(tag uint16, fid uint32, stat Stat) {
 	if len(stat) > maxStatLen {
-		return errLongStat
+		panic(errLongStat)
 	}
 	if len(stat) < minStatLen {
-		return errShortStat
+		panic(errShortStat)
 	}
 	size := uint32(minSizeLUT[msgTwstat] + len(stat))
 
@@ -465,16 +426,14 @@ func (enc *Encoder) Twstat(tag uint16, fid uint32, stat Stat) error {
 
 	pheader(tx, size, msgTwstat, tag)
 	pbyte(tx, stat)
-	return enc.Err()
 }
 
 // Rwstat writes an Rwstat message to the underlying io.Writer.
-func (enc *Encoder) Rwstat(tag uint16) error {
+func (enc *Encoder) Rwstat(tag uint16) {
 	size := uint32(maxSizeLUT[msgRwstat])
 
 	tx := enc.w.Tx()
 	defer tx.Close()
 
 	pheader(tx, size, msgRwstat, tag)
-	return enc.Err()
 }
