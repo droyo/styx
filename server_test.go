22e04377a5cecef374d169438d37314b7f912db8
David Arroyo <droyo@aqwari.net>
2016-09-12 22:23:10 -0400

    Revamp tests

--- server_test.go
+++ server_test.go
@@ -2,89 +2,112 @@ package styx
 
 import (
 	"io"
-	"net"
 	"os"
-	"path/filepath"
 	"testing"
-	"time"
 
 	"aqwari.net/net/styx/internal/netutil"
 	"aqwari.net/net/styx/styxproto"
 )
 
-type dialee interface {
-	Dial() (net.Conn, error)
+type testLogger struct {
+	*testing.T
 }
 
-type testLogger testing.T
-
-func (t *testLogger) Printf(format string, args ...interface{}) {
+func (t testLogger) Printf(format string, args ...interface{}) {
 	t.Logf(format, args...)
 }
 
-func sendClientTraffic(t *testing.T, ln *netutil.PipeListener) {
-	c, err := ln.Dial()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	file, err := os.Open(filepath.Join("styxproto", "testdata", "sample.client.9p"))
-	if err != nil {
-		t.Error(err)
-		return
-	}
+type callback func(styxproto.Msg)
 
-	go func() {
-		io.Copy(c, file)
-		time.Sleep(time.Second)
-		c.Close()
-	}()
-
-	d := styxproto.NewDecoder(c)
-	for d.Next() {
-		t.Logf("%d %s", d.Msg().Tag(), d.Msg())
-	}
-	if d.Err() != nil {
-		t.Log(d.Err())
-	}
+type testServer struct {
+	send, recv callback
+	handler    Handler
+	test       *testing.T
 }
 
-func TestServerBasic(t *testing.T) {
-	srv := Server{
-		ErrorLog: (*testLogger)(t),
-		TraceLog: (*testLogger)(t),
+func openfile(filename string) (*os.File, func()) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
 	}
+	return file, func() { file.Close() }
+}
+
+func (s testServer) run(input io.Reader) {
 	var ln netutil.PipeListener
 	defer ln.Close()
 
-	go func() {
-		t.Log(srv.Serve(&ln))
-	}()
-	sendClientTraffic(t, &ln)
-}
-
-func TestEchoServer(t *testing.T) {
-	srv := Server{
-		ErrorLog: (*testLogger)(t),
-		TraceLog: (*testLogger)(t),
+	if s.send == nil {
+		s.send = func(styxproto.Msg) {}
 	}
-	var ln netutil.PipeListener
-	defer ln.Close()
+	if s.recv == nil {
+		s.recv = func(styxproto.Msg) {}
+	}
+	srv := Server{
+		Handler:  s.handler,
+		ErrorLog: testLogger{s.test},
+	}
+	go srv.Serve(&ln)
 
-	srv.Handler = HandlerFunc(func(s *Session) {
-		for s.Next() {
-			msg := s.Request()
-			t.Logf("%T %s", msg, msg.Path())
-			switch msg := msg.(type) {
-			case Topen:
-				msg.Ropen(os.Open(os.DevNull))
-			default:
-				msg.defaultResponse()
-			}
+	conn, _ := ln.Dial()
+	in := styxproto.NewDecoder(input)
+	out := styxproto.NewDecoder(conn)
+
+	step := make(chan struct{})
+	go func() {
+		for out.Next() {
+			s.test.Logf("← %03d %s", out.Msg().Tag(), out.Msg())
+			s.recv(out.Msg())
+			step <- struct{}{}
 		}
-	})
-	go func() {
-		t.Log(srv.Serve(&ln))
+		if err := out.Err(); err != nil {
+			s.test.Error(err)
+		} else {
+			s.test.Log("server closed connection")
+		}
+		close(step)
+
+		// Force client to stop writing when server is
+		// done
+		conn.Close()
 	}()
-	sendClientTraffic(t, &ln)
+
+Loop:
+	for in.Next() {
+		s.test.Logf("→ %03d %s", in.Msg().Tag(), in.Msg())
+		if _, err := styxproto.Write(conn, in.Msg()); err != nil {
+			break Loop
+		}
+		s.send(in.Msg())
+		if _, ok := <-step; !ok {
+			break Loop
+		}
+	}
+	conn.Close()
+	if err := out.Err(); err != nil {
+		s.test.Error(err)
+	}
+}
+
+func (s testServer) runMsg(fn func(*styxproto.Encoder)) {
+	rd, wr := io.Pipe()
+	e := styxproto.NewEncoder(wr)
+	go func() {
+		e.Tversion(styxproto.DefaultMaxSize, "9P2000")
+		fn(e)
+		e.Flush()
+		wr.Close()
+	}()
+	s.run(rd)
+}
+
+func (s testServer) runFile(inputfile string) {
+	file, cleanup := openfile(inputfile)
+	defer cleanup()
+	s.run(file)
+}
+
+func TestBasic(t *testing.T) {
+	s := testServer{test: t}
+	s.runFile("styxproto/testdata/sample.client.9p")
 }
